864c3f60267a37382c5053d3991baf3acfa7d037
sweetrain096 <gpfhddl09@gmail.com>
2019-03-19 01:37:02 +0000

    06|CRUD-C

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import BoardForm
 
 # Create your views here.
 def index(request):
     boards = Board.objects.order_by('-pk')
     context = {'boards' : boards}
     return render(request, 'boards/index.html', context)
+    
+def create(request):
+    if request.method == 'POST':
+        '''
+        # 앞으로는 request.POST를 사용하지 않을것.
+        # 검증이 안되기 때문
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        board = Boad(title=title, content=content)
+        board.save()
+        '''
+        board_form = BoardForm(request.POST)
+        if board_form.is_valid():
+            title = board_form.cleaned_data.get('title')
+            content = board_form.cleaned_data.get('content')
+            board = Board(title=title, content=content)
+            board.save()
+            return redirect('boards:index')
+    else:
+        board_form = BoardForm()
+    context = {'board_form' : board_form}
+    return render(request, 'boards/create.html', context)
