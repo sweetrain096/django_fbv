7de7e46ba9d580e039037a32ab6dd6b478702374
sweetrain096 <gpfhddl09@gmail.com>
2019-03-19 02:18:18 +0000

    08|CRUD-D

--- boards/urls.py
+++ boards/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.create, name='create'),
     path('<int:board_pk>/', views.detail, name='detail'),
+    path('<int:board_pk>/delete/', views.delete, name='delete'),
 ]
